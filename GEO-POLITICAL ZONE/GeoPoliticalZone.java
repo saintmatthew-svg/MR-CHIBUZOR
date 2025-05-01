bb9698c0dcd449310ebb723b1105cf6d427fb7e1
saintmatthew-svg <onyekachimatthew00@gmail.com>
2025-05-01 13:47:56 +0100

    commited

---
+++ GEO-POLITICAL ZONE/GeoPoliticalZone.java
@@ -0,0 +1,27 @@
+public enum GeoPoliticalZone {
+    NORTH_CENTRAL("Benue", "FCT", "Kogi", "Kwara", "Nasarawa", "Niger", "Plateau"),
+    NORTH_EAST("Adamawa", "Bauchi", "Borno", "Gombe", "Taraba", "Yobe"),
+    NORTH_WEST("Kaduna", "Katsina", "Kano", "Kebbi", "Sokoto", "Jigawa", "Zamfara"),
+    SOUTH_EAST("Abia", "Anambra", "Ebonyi", "Enugu", "Imo"),
+    SOUTH_SOUTH("Akwa-Ibom", "Bayelsa", "Cross-River", "Delta", "Edo", "Rivers"),
+    SOUTH_WEST("Ekiti", "Lagos", "Osun", "Ondo", "Ogun", "Oyo");
+
+    private final String[] states;
+
+    GeoPoliticalZone(String... states) {
+        this.states = states;
+    }
+
+    public String[] getStates() {
+        return states;
+    }
+
+    public int getNumStates() {
+        for(GeoPoliticalZone zone : GeoPoliticalZone.values()) {
+            if(zone.getStates().length == states.length) {
+                return zone.getStates().length;
+            }
+        }
+        return 0;
+    }
+}
